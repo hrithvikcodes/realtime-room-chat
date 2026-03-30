94ee58ce5591bcb2594d515f6406060263361dbc
Hrithvik <hrithvikmypali@gmail.com>
2026-03-30 11:12:48 +0530

    fix: (ws) correct connect() call

--- app/routers/websocket.py
+++ app/routers/websocket.py
@@ -9,32 +9,30 @@ router = APIRouter()
 
 @router.websocket("/ws/{room_id}")
 async def chat_socket(websocket: WebSocket, room_id:UUID, token: str = Query(...)):
+    
     try:
-        user = await get_user_from_token(token)
+        async with get_db_context() as db:
+            user = await get_user_from_token(token, "access",db)
+            
+
     except Exception:
         await websocket.close(code=403)
         return
     
-    await manager.connect(websocket,room_id)
-
-    try:
+    await manager.connect(websocket, room_id)
+    try: 
         while True:
             data = await websocket.receive_json()
+            content = data.get("content")
+            if not content:
+                continue
             async with get_db_context() as db:
-                new_msg = await send_message(
-                    room_id=room_id,
-                    user_id=user.id,
-                    db = db,
-                    content=data["content"],
-                    file=None
-                )
-                await manager.broadcast_to_room(
-                    room_id,{
-                        "sender": user.name,
-                        "content": data.get("content"),
-                        "created_at": str(new_msg.posted_at),
-                        "media_url": new_msg.media_url,
-                    }
-                )
+                new_msg = await send_message(room_id = room_id, user_id = user.id, db = db , content = content, file = None)
+            await manager.broadcast_to_room(room_id, {
+                "sender" : user.name,
+                "content": content,
+                "created_at": str(new_msg.posted_at),
+                "media_url": new_msg.media_url
+            })
     except WebSocketDisconnect:
-        manager.disconnect(websocket,room_id)
+        manager.disconnect(websocket, room_id)
