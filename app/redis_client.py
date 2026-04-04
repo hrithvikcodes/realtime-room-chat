7e90b3a68a549610e0d7c58dffac1c880e51ce15
Hrithvik <hrithvikmypali@gmail.com>
2026-04-05 00:13:51 +0530

    use env var for Redis URL

--- app/redis_client.py
+++ app/redis_client.py
@@ -1,9 +1,6 @@
 import redis.asyncio as redis
+import os
 
+REDIS_URL = os.getenv("REDIS_URL", "redis://localhost:6379")
 
-redis_client = redis.Redis(
-    host="localhost",
-    port=6379,
-    db=0,
-    decode_responses=True
-)
+redis_client = redis.from_url(REDIS_URL, decode_responses=True)
